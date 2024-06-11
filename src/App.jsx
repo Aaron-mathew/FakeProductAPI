808fc1366e9b94d0f4a0627bd4973731b67e242f
Aaron Mathew <aaronmathew453@gmail.com>
2024-06-11 17:36:53 +0530

    made navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import React from 'react'
 const App = () => {
   return (
     <div className='h-screen w-screen'>
-      <nav className='w-[15%] h-full bg-zinc-100 flex flex-col items-center pt-5'>
-        <a className='py-3 px-5 border rounded' href="/create">Add New Product</a>
-        <hr className='w-[80%]'/>
-        <h1>Category</h1>
-        <ul>
-          <li>Cat 1</li>
+      <nav className='w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5'>
+        <a className='py-2 px-5 border rounded border-blue-200 text-blue-300' href="/create">Add New Product</a>
+        <hr className='my-3 w-[80%]'/>
+        <h1 className='text-2xl mb-3 w-[80%]'>Category Filter</h1>
+        <ul className='w-[80%]'>
+          <li className=' flex items-center mb-3'>
+            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-blue-100'></span>
+            Cat 1
+          </li>
+          <li className=' flex items-center mb-3'>
+            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-red-100'></span>
+            Cat 1
+          </li>
+          <li className=' flex items-center mb-3'>
+            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-green-100'></span>
+            Cat 1
+          </li>
         </ul>
       </nav>
     </div>
@@ -17,4 +28,4 @@ const App = () => {
 
 export default App;
 
-// {04:58 Video}
+// {12:20 Video}
