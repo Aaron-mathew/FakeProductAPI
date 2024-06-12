e0c4347e3071b6764dfc5a2ddd6a7866d0a5cf0e
Aaron Mathew <aaronmathew453@gmail.com>
2024-06-12 23:21:37 +0530

    Made card ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='h-screen w-screen'>
+    <div className='h-screen w-screen flex'>
       <nav className='w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5'>
         <a className='py-2 px-5 border rounded border-blue-200 text-blue-300' href="/create">Add New Product</a>
         <hr className='my-3 w-[80%]'/>
@@ -22,10 +22,17 @@ const App = () => {
           </li>
         </ul>
       </nav>
+      <div className=' w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto'>
+        <div className='card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center'>
+          <div className='hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center' style={{backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)" }}></div>
+          <h1>Lorem ipsum dolor sit amet.</h1>
+        </div>
+        
+      </div>
     </div>
   )
 }
 
 export default App;
 
-// {12:20 Video}
+// {25:39 Video}
