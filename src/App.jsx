b575a0e2212a564df9d8142b4411d4a534ead8d0
Aaron Mathew <aaronmathew453@gmail.com>
2024-06-13 22:56:33 +0530

    Implemeted Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div className='h-screen w-screen flex'>
-      <nav className='w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5'>
-        <a className='py-2 px-5 border rounded border-blue-200 text-blue-300' href="/create">Add New Product</a>
-        <hr className='my-3 w-[80%]'/>
-        <h1 className='text-2xl mb-3 w-[80%]'>Category Filter</h1>
-        <ul className='w-[80%]'>
-          <li className=' flex items-center mb-3'>
-            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-blue-100'></span>
-            Cat 1
-          </li>
-          <li className=' flex items-center mb-3'>
-            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-red-100'></span>
-            Cat 1
-          </li>
-          <li className=' flex items-center mb-3'>
-            <span className='rounded-full mr-2 w-[15px] h-[15px] bg-green-100'></span>
-            Cat 1
-          </li>
-        </ul>
-      </nav>
-      <div className=' w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto'>
-        <div className='card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center'>
-          <div className='hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center' style={{backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)" }}></div>
-          <h1>Lorem ipsum dolor sit amet.</h1>
-        </div>
-        
-      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+
+
     </div>
   )
 }
 
 export default App;
 
-// {25:39 Video}
+// {30:49 Video}
