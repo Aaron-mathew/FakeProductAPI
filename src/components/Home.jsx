b897aa9fc382aa515703b5c34c04be174af5ce5e
Aaron Mathew <aaronmathew453@gmail.com>
2024-06-18 20:53:38 +0530

    Products are now visible in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Nav from './Nav'
 import { Link } from 'react-router-dom'
-
+import { ProductContext } from '../utils/Context'
+import Loading from './Loading'
+ 
 const Home = () => {
-  return (
+  const [products] = useContext(ProductContext);
+  return products ? (
     <>
     <Nav />
         <div className=' w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto'>
-            <Link to="/details/1" className='card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center'>
-                <div className='hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center' style={{backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)" }}></div>
-                <h1 className='hover:text-blue-300'>Lorem ipsum dolor sit amet.</h1>
-            </Link>
+
+          {products.map((p,i) => (            
+          <Link to="/details/1" className='card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center'>
+                <div className='hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center' style={{backgroundImage: `url(${p.image})`, }}></div>
+                <h1 className='hover:text-blue-300'>{p.title}</h1>
+            </Link> ))}
+
         </div>
     </>
-  )
-}
+    ):( <Loading />
+  );
+};
 
 export default Home
+
+
+
+// {1:01:52 Video}
--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -6,4 +6,3 @@ const instance = axios.create({
 
 export default instance;
 
-// {52:10 Video}
