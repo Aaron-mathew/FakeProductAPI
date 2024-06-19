7143fb8ba7545403fc98498fdb3e6d659ac4540b
Aaron Mathew <aaronmathew453@gmail.com>
2024-06-19 23:38:14 +0530

    Made the Details page dynamic.

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import axios from '../utils/axios';
+import Loading from './Loading';
 
 const Details = () => {
-  return (
+  const [product, setproduct] = useState(null);
+  const {id} = useParams();
+  console.log(id);
+  const getsingleproduct = async () => {
+    try {
+      const {data} = await axios.get(`/products/${id}`);
+      setproduct(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getsingleproduct();
+  }, []);
+  
+  return ( product?
     <div className='w-[70%] flex h-full justify-between items-center m-auto p-[10%]'>
-        <img className='object-contain h-[80%] w-[40%]' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
+        <img className='object-contain h-[80%] w-[40%]' src={`${product.image}`} alt="" />
         <div className='content w-[50%]'>
-            <h1 className='text-4xl'>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
-            <h3 className='text-zinc-400 my-5'>men's clothing</h3>
-            <h2 className='text-red-300 mb-3'>$ 109.95</h2>
-            <p className='mb-[5%]'>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+            <h1 className='text-4xl'>{product.title}</h1>
+            <h3 className='text-zinc-400 my-5'>{product.category}</h3>
+            <h2 className='text-red-300 mb-3'>$ {product.price}</h2>
+            <p className='mb-[5%]'>{product.description}</p>
             <Link className=' mr-5 py-2 px-5 border rounded border-blue-200 text-blue-300'>Edit</Link>
             <Link className='py-2 px-5 border rounded border-red-200 text-red-300'>Delete</Link>
         </div>
-    </div>
-  )
-}
+    </div>: <Loading />
+  );
+};
 
 export default Details
 
+
+// {1:08:55 Video}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <div className=' w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto'>
 
           {products.map((p,i) => (            
-          <Link to="/details/1" className='card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center'>
+        <Link to={`/details/${p.id}`}className='card mr-3 mb-3 p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center'>
                 <div className='hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center' style={{backgroundImage: `url(${p.image})`, }}></div>
                 <h1 className='hover:text-blue-300'>{p.title}</h1>
             </Link> ))}
@@ -26,5 +26,3 @@ const Home = () => {
 export default Home
 
 
-
-// {1:01:52 Video}
